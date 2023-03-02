15e3e4b59823ca4e588f3f777d1c21010ec79dbc
Guilherme Souza <guilhermejcamargos@gmail.com>
2023-03-02 12:29:27 -0300

    pipes, emitter e change number

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { ParentDataComponent } from './components/parent-data/parent-data.compon
 import { DirectivesComponent } from './components/directives/directives.component';
 import { IfRenderComponent } from './components/if-render/if-render.component';
 import { EventosComponent } from './components/eventos/eventos.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +19,11 @@ import { EventosComponent } from './components/eventos/eventos.component';
     ParentDataComponent,
     DirectivesComponent,
     IfRenderComponent,
-    EventosComponent
+    EventosComponent,
+    EmitterComponent,
+    ChangeNumberComponent,
+    ListRenderComponent,
+    PipesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/change-number/change-number.component.ts
@@ -0,0 +1,14 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-change-number',
+  templateUrl: './change-number.component.html',
+  styleUrls: ['./change-number.component.css'],
+})
+export class ChangeNumberComponent {
+  @Output() changeNumber = new EventEmitter<any>();
+
+  handleClick() {
+    this.changeNumber.emit();
+  }
+}
